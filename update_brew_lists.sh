423b9bdf1e5a91db9f13fe4353df1954ac87474c
Mendi Levin <mlevin@crossriver.com>
2023-10-13 12:35:08 +0300

    all

--- update_brew_lists.sh
+++ update_brew_lists.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Navigate to the directory where your brew lists and git repo are
-cd ~/Documents/MyProject/GitHubRepo/homebrew
+cd /Users/mlevin/Documents/MyProject/GitHubRepo/homebrew
 
 # Update brew and cask lists
 echo "Updating brew and cask lists..."
-brew list --formula > ~/Documents/MyProject/GitHubRepo/homebrew/brew_packages.txt
+brew list --formula > /Users/mlevin/Documents/MyProject/GitHubRepo/homebrew/brew_packages.txt
 echo "brew_packages.txt updated"
 
 echo "Updating brew casks..."
-brew list --cask > ~/Documents/MyProject/GitHubRepo/homebrew/brew_casks.txt
+brew list --cask > /Users/mlevin/Documents/MyProject/GitHubRepo/homebrew/brew_casks.txt
 echo "brew_casks.txt updated"
 
 # Git operations
