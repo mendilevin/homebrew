f0c41479e12a9d949e02674c442c8ca70ad62271
Mendi Levin <mlevin@crossriver.com>
2023-10-13 12:24:12 +0300

    all

--- update_brew_lists.sh
+++ update_brew_lists.sh
@@ -5,11 +5,11 @@ cd ~/Documents/MyProject/GitHubRepo/homebrew
 
 # Update brew and cask lists
 echo "Updating brew and cask lists..."
-brew list --formula > brew_packages.txt
+brew list --formula > ~/Documents/MyProject/GitHubRepo/homebrew/brew_packages.txt
 echo "brew_packages.txt updated"
 
 echo "Updating brew casks..."
-brew list --cask > brew_casks.txt
+brew list --cask > ~/Documents/MyProject/GitHubRepo/homebrew/brew_casks.txt
 echo "brew_casks.txt updated"
 
 # Git operations
